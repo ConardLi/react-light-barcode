77595605574a54a82e2259eca4c32b5bc5d7b92d
shiqi.li <1009903985@qq.com>
2018-10-16 01:19:06 +0800

    updateExample

--- example/index.js
+++ example/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Code from './Code';
+import { Code128 } from '../lib/index';
+
+const TRUE = true;
 
 ReactDOM.render(
-  <Code />,
+  <Code128
+    value='1234567891234'
+    displayValue={TRUE}
+    size={1}
+    type='C'
+    color='black'
+    fontSize='0.125in'
+    fontColor='black'
+  />,
   document.getElementById('root')
 );
